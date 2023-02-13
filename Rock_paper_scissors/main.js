ebc14c46fd595c1fb5abda1e8a7e662c834ff648
Gurupreet Singh <gusi@odoo.com>
2023-02-13 17:10:46 +0530

    Improving the code on basis of PR commits

--- Rock_paper_scissors/main.js
+++ Rock_paper_scissors/main.js
@@ -3,26 +3,14 @@ let wins = 0;
 let losses = 0;
 let ties = 0;
 
-function ABC(value) {
+function gameState(value) {
     var rand = arr[Math.floor(Math.random() * 3)];
     document.getElementById("print_result").innerHTML = `You choose ${value}, computer choose ${rand}.<br/><br/>`;
-
-    if (value == 'rock' && rand == "scissor" ||
-        value == "paper" && rand == "rock" ||
-        value == "scissor" && rand == "paper") {
-        document.getElementById("win").innerHTML = wins += 1;
-        document.getElementById("print_result").innerHTML += `You Win`;
-
-    } else if (rand == 'rock' && value == "scissor" ||
-        rand == "paper" && value == "rock" ||
-        rand == "scissor" && value == "paper") {
-        document.getElementById("lose").innerHTML = losses += 1;
-        document.getElementById("print_result").innerHTML += `Computer Win`;
-
-    } else {
-        document.getElementById("tie").innerHTML = ties += 1;
-        document.getElementById("print_result").innerHTML += `it's a Tie`;
-
-    }
-
+    let you_win = ((value == 'rock' && rand == "scissor") || (value == "paper" && rand == "rock") || (value == "scissor" && rand == "paper"))
+    let computer_win = (value == 'rock' && value == "scissor" || rand == "paper" && value == "rock" || rand == "scissor" && value == "paper")
+    document.getElementById("print_result").innerHTML += (you_win) ? `You Win`  : (value === rand) ? `it's a Tie` : `Computer Win`
+    incrementScore(you_win,computer_win)
 }
+const incrementScore = ((user,computer) => {
+        user ? document.getElementById("win").innerHTML = wins += 1 : computer ? document.getElementById("lose").innerHTML = losses += 1 : document.getElementById("tie").innerHTML = ties +=1;
+    });
